f7ef584987c45466548395a99ce2be5a5704ad51
Shaurya Thareja <vankap.grover@gmail.com>
2024-09-29 16:40:33 -0700

    Added specification of Player class + Renamed Sample files

---
+++ src/main/model/Player.java
@@ -0,0 +1,95 @@
+package model;
+
+/**
+ * Represents a player in the table tennis tournament, with statistics
+ * such as points won/lost, matches won/lost, overall ability,ranking points, 
+ * and information such as name, age, country of origin, and match history.
+ */
+public class Player {
+
+    // REQUIRES: age > 0 && overallAbility > 0
+    // EFFECTS: Creates a new Player object with given name,
+    //          age, overall ability, and country of origin,
+    //          sets the ranking table points to 0, and sets
+    //          the matches and points win-loss records to 0-0 each.
+    public Player(String name, int age, int overallAbility String country) {
+        // stub
+    }
+
+    public String getName() {
+        return ""; // stub
+    }
+
+    public int getAge() {
+        return 0; // stub
+    }
+
+    public String getCountry() {
+        return ""; // stub
+    }
+
+    public int getPointsWon() {
+        return 0; // stub
+    }
+
+    public int getPointsConceeded() {
+        return 0; // stub
+    }
+
+    public int getPointsDifference() {
+        return 0; // stub
+    }
+
+    public int getMatchesWon() {
+        return 0; // stub
+    }
+
+    public int getMatchesLost() {
+        return 0; // stub
+    }
+
+    public int getRankingPoints() {
+        return 0; // stub
+    }
+
+    public int getOverallAbility() {
+        return 0; // stub
+    }
+
+    public boolean isEliminated() {
+        return false; // stub
+    }
+
+    public ArrayList<Match> getMatchHistory() {
+        return null; // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: increments the number of matches won and 
+    //          adds 2 to the ranking points
+    public void winMatch() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: increments the number of matches lost
+    public void loseMatch() {
+        // stub
+    }
+
+    // REQUIRES: points > 0
+    // MODIFIES: this
+    // EFFECTS: increases the number of points won by 
+    //          given points
+    public void addPoints(int points) {
+        // stub
+    }
+
+    // REQUIRES: points > 0
+    // MODIFIES: this
+    // EFFECTS: decreases the number of points conceeded 
+    //          by given points
+    public void concedePoints(int points) {
+        // stub
+    }
+}
--- src/test/model/PlayerTest.java
+++ src/test/model/PlayerTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class TestSample {
+public class PlayerTest {
     
     @BeforeEach
     void runBefore() {
