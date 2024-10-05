6cb32cca2355b1920d78472d7f232069645f86f4
Shaurya Thareja <shauryathareja19@gmail.com>
2024-10-04 23:20:23 -0700

    Added specification for RankingTable class

--- src/main/model/RankingTable.java
+++ src/main/model/RankingTable.java
@@ -1,5 +1,41 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Represents a ranking table for a tournament that ranks
+ * all the players based on their statistics.
+ */
 public class RankingTable {
 
+    // REQUIRES: listOfPlayers.size() > 0
+    // EFFECTS: Creates a ranking table of all given players, 
+    //          that is initially listed alphabetically.
+    public RankingTable(ArrayList<Player> listOfPlayers) {
+        // stub
+    }
+
+    public HashMap<String, Integer> getRankings() {
+        return null; // stub
+    }
+
+    public int getPlayerRanking(String playerName) {
+        return 0; // stub
+    }
+
+    public String getPlayerAtRank(int rank) {
+        return ""; // stub
+    }
+
+    public HashMap<String, Integer> getTopPlayers(int numTopPlayers) {
+        return null; // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: updates the ranking table according to the players'
+    //          latest statistics since the last call to this method.
+    public void updateRankings() {
+        // stub
+    }
 }
