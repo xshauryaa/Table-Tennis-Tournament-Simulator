b8a252b2f6d35625f44dff0bc5fa7504e7c5d11c
Shaurya Thareja <shauryathareja19@gmail.com>
2024-10-29 22:18:28 -0700

    Added specification for TableTennisTournamentSimulatorApp class (GUI main frame class)

---
+++ src/main/ui/TableTennisTournamentSimulatorApp.java
@@ -0,0 +1,27 @@
+package ui;
+
+import javax.swing.*;
+
+// Referenced by JsonSerializationDemo
+// https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
+
+// Represents the main window of the GUI of the app
+public class TableTennisTournamentSimulatorApp extends JFrame {
+    
+    // EFFECTS: sets up window in which Table Tennis Tournament Simulator App will be played
+    public TableTennisTournamentSimulatorApp() {
+        // stub
+    }
+
+	// EFFECTS: initializes a timer that updates game each
+	//          few milliseconds
+    private void addTimer() {
+		// stub
+    }
+
+	// MODIFIES: this
+	// EFFECTS: location of frame is set so frame is centred on desktop
+    private void centreOnScreen() {
+		// stub
+    }
+}
