0f900df4f4d67bf174240c1bda71846ea66da924
Shaurya Thareja <shauryathareja19@gmail.com>
2024-11-13 16:27:10 -0800

    Added specification for PlayOpeningMatchesPanel class

--- src/main/ui/panels/PlayOpeningMatchesPanel.java
+++ src/main/ui/panels/PlayOpeningMatchesPanel.java
@@ -2,7 +2,29 @@ package ui.panels;
 
 import javax.swing.*;
 
+import ui.StyleGuide;
+import ui.TableTennisTournamentSimulatorApp;
+
 // Represents the panel where user simulates opening matches
 public class PlayOpeningMatchesPanel extends JPanel {
     
+    // EFFECTS: draws the panel where opening matches are played
+    public PlayOpeningMatchesPanel(TableTennisTournamentSimulatorApp owner) {
+        // stub
+    }
+
+    // EFFECTS: makes a label for "Opening Round" and adds it to the screen
+    private void addTitleLabel() {
+        // stub
+    }
+
+    // EFFECTS: makes a JButton to simulate matches and adds it to the screen
+    private void addSimulateButton() {
+        // stub
+    }
+
+    // EFFECTS: creates the panel for displaying all the matches in the opening round
+    private void addMatchDisplay() {
+        // stub
+    }
 }
