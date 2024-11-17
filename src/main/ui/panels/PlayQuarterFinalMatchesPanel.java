e00989a99a8c5d36a273b607041cb01229794538
Shaurya Thareja <shauryathareja19@gmail.com>
2024-11-17 02:30:09 -0800

    Added specification for PlayQuarterFinalMatchesPanel class

--- src/main/ui/panels/PlayQuarterFinalMatchesPanel.java
+++ src/main/ui/panels/PlayQuarterFinalMatchesPanel.java
@@ -2,7 +2,29 @@ package ui.panels;
 
 import javax.swing.*;
 
+import ui.StyleGuide;
+import ui.TableTennisTournamentSimulatorApp;
+
 // Represents the panel where user simulates quarter final matches
 public class PlayQuarterFinalMatchesPanel extends JPanel {
     
+    // EFFECTS: draws the panel where opening matches are played
+    public PlayQuarterFinalMatchesPanel(TableTennisTournamentSimulatorApp owner, SideMenuPanel smp) {
+        // stub
+    }
+
+    // EFFECTS: makes a label for "Quarter Finals" and adds it to the screen
+    private void addTitleLabel() {
+        // stub
+    }
+
+    // EFFECTS: makes a JButton to simulate matches and adds it to the screen
+    private void addSimulateButton() {
+        // stub
+    }
+
+    // EFFECTS: creates the panel for displaying all the matches in the quarter finals
+    private void addMatchDisplay() {
+        // stub
+    }
 }
