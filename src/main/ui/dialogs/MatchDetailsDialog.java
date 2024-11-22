7309ab41eed426dfa441e2ac0b07b73b1c34e40e
Shaurya Thareja <shauryathareja19@gmail.com>
2024-11-22 02:22:36 -0800

    Added specification for MatchDetailsDialog class

--- src/main/ui/dialogs/MatchDetailsDialog.java
+++ src/main/ui/dialogs/MatchDetailsDialog.java
@@ -2,7 +2,35 @@ package ui.dialogs;
 
 import javax.swing.*;
 
+import model.Match;
+import ui.TableTennisTournamentSimulatorApp;
+
 // Represents the dialog box that shows the details of given match
 public class MatchDetailsDialog extends JDialog {
     
+    // EFFECTS: creates a dialog that shows details of given match
+    public MatchDetailsDialog(TableTennisTournamentSimulatorApp owner, Match m) {
+        // stub
+    }
+
+    // EFFECTS: creates a panel with label for "Match <MatchID>" and adds it to the dialog
+    private void addMatchID() {
+        // stub
+    }
+
+    // EFFECTS: creates a panel with display for players and adds it to the dialog
+    private void addPlayerDisplay() {
+        // stub
+    }
+
+    // EFFECTS: creates a panel with set scores and winner for match and adds it to the dialog
+    private void addDetailsPanel() {
+        // stub
+    }
+
+    // MODIFIES: this
+	// EFFECTS: location of frame is set so frame is centred on desktop
+    private void centreOnScreen() {
+        // stub
+    }
 }
